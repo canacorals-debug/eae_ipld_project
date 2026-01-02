5a1610fe7be98075aec0c506df947a0940dca258
Julio Ramirez <julio.jesus010399@gmail.com>
2026-01-02 15:22:37 -0500

    3.1 update. Trying to delete extra table by removing any data set stand along code

--- pages/03_temperatures_dashboard.py
+++ pages/03_temperatures_dashboard.py
@@ -23,11 +23,12 @@ st.divider()
 @st.cache_data
 def load_data():
     data_path = "data/cities_temperatures.csv"
-   
-    # TODO: Ex 3.1: Load the dataset using Pandas, use the data_path variable and set the index column to "show_id"
 
+    # Load dataset and set 'show_id' as index
     temps_df = pd.read_csv(data_path, index_col="show_id")
-    st.dataframe(temps_df.head(5))  # only first 5 rows shown
+
+    # Display only the first 5 rows
+    st.dataframe(temps_df.head(5)) # only first 5 rows shown
 
     if temps_df is not None:
         temps_df["Date"] = pd.to_datetime(temps_df["Date"]).dt.date
