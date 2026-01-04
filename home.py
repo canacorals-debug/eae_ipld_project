00e1e8e8e263b19f7590267d55aeada74fbc9f01
Julio Ramirez <julio.jesus010399@gmail.com>
2026-01-04 11:03:32 -0500

    Name change

--- home.py
+++ home.py
@@ -21,7 +21,7 @@ def home_page():
 
 
     # ----- Top title -----
-    st.html("""<div style="text-align: center;"><h1 style="text-align: center;">👋 Hi! My name is Enric</h1></div>""")  # TODO: Add your name
+    st.html("""<div style="text-align: center;"><h1 style="text-align: center;">👋 Hi! My name is Julio Ramirez</h1></div>""")  # TODO: Add your name
 
 
     # ----- Profile image file -----
