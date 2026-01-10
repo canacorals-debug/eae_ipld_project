9c0668eb0af733c03c042be3e5794677492c7755
Julio Ramirez <julio.jesus010399@gmail.com>
2026-01-10 12:52:10 -0400

    author name

--- home.py
+++ home.py
@@ -16,7 +16,7 @@ def home_page():
         st.header("Introduction to Programming Languages for Data")
         st.write("###")
         st.write("***Final Project - Dec 2025***")
-        st.write("**Author:** <your name> ")
+        st.write("**Author:** <Julio Jesus Ramirez> ")
         st.write("**Instructor:** [Enric Domingo](https://github.com/enricd)")
 
 
